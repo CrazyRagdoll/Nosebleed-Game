8c16ba865168ac95dca627976d5407df05bd56ed
Sam Bowen <Samuel_bowen_1994@hotmail.co.uk>
2019-04-04 17:47:23 +0100

    Inital Commit - base game with simple player movement and level

---
+++ NoseBleed Code Test/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject CameraTarget;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Simple follow the player for now
+        transform.position = new Vector3(CameraTarget.GetComponent<Transform>().position.x, 
+                                        CameraTarget.GetComponent<Transform>().position.y, 
+                                        -10.0f);
+    }
+}
---
+++ NoseBleed Code Test/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public float moveSpeed = 40.0f;
+
+    float horizonalMove = 0.0f;
+
+    bool jump = false;
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Horizontal movement
+        horizonalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        //Jump input
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        //Crouching implementation -- Probably wont be used
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //Move our player with the controller script
+        //Using Time.fixedDeltaTime allows us to have a consistant movespeed across all platforms regardless of how often update is called
+        controller.Move(horizonalMove * Time.fixedDeltaTime, crouch, jump);
+
+        //Reset the jump variable
+        jump = false;
+    }
+}
