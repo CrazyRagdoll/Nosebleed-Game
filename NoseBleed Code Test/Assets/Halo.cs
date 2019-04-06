705dd6e0134284adda41c1845370d7c0ccc8a170
Sam Bowen <Samuel_bowen_1994@hotmail.co.uk>
2019-04-06 03:34:23 +0100

    Added new weapon type :+1:

---
+++ NoseBleed Code Test/Assets/Halo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Halo : Weapon
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireMode = new FireMode(ShotType.HALO, 50, 80, 250, 25, 8, 15);
+    }
+}
