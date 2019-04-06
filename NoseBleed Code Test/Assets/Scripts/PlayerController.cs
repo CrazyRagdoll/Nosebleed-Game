c571b0626ab6cdeb709dce7a9f427ce567b0f3b3
Sam Bowen <Samuel_bowen_1994@hotmail.co.uk>
2019-04-06 02:55:26 +0100

    Now creates a dictonary of weapons with their ID values and can cycle through all of the aquired weapons with q and e :D

--- NoseBleed Code Test/Assets/Scripts/PlayerController.cs
+++ NoseBleed Code Test/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,10 @@ public class PlayerController : MonoBehaviour
     public int Health = 100;
 
     public Weapon activeWeapon;
+    static int activeWeaponID = 0;
+    static int numWeapons = 0;
+
+    public Dictionary<int, Weapon> weapons = new Dictionary<int, Weapon>();
 
     // Update is called once per frame
     void Update()
@@ -16,6 +20,48 @@ public class PlayerController : MonoBehaviour
         if(Health <= 0)
         {
             //Game over
+        }
+        
+        //Cycle through the weapons
+        if (Input.GetButtonDown("CycleDown"))
+        {
+            SetActiveWeapon(-1);
+        }
+        if (Input.GetButtonDown("CycleUp"))
+        {
+            SetActiveWeapon(1);
+        }
+    }
+
+    private void SetActiveWeapon(int next)
+    {
+        //If we have more than 1 weapon
+        if(weapons.Count > 1)
+        {
+            //Disable the current weapon
+            weapons[activeWeaponID].isActive = false;
+            weapons[activeWeaponID].GetComponent<SpriteRenderer>().enabled = false;
+
+            //If we're going too high out of bounds
+            if ((activeWeaponID + next) == numWeapons)
+            {
+                activeWeaponID = 0;
+            }   
+            //Too low out of bounds...
+            else if ((activeWeaponID + next) < 0)
+            {
+                activeWeaponID = numWeapons - 1;
+            }
+            //otherwise it's OK!
+            else
+            {
+                activeWeaponID += next;
+            }
+
+            //Enable the new weapon
+            weapons[activeWeaponID].isActive = true;
+            weapons[activeWeaponID].GetComponent<SpriteRenderer>().enabled = true;
+            activeWeapon = weapons[activeWeaponID];
         }
     }
 
@@ -30,23 +76,32 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Weapon weapon = collision.GetComponent<Weapon>();
-        if (weapon != null)
+        Weapon gun = collision.GetComponent<Weapon>();
+        if (gun != null)
         {
             //Add it to the list of guns
-            weapon.transform.SetParent(transform.Find("Guns"));
+            gun.transform.SetParent(transform.Find("Guns"));
 
             //Set its position on the player
-            weapon.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
-            weapon.transform.localPosition -= (weapon.handlePoint.transform.localPosition/2);
+            gun.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+            gun.transform.localPosition -= (gun.handlePoint.transform.localPosition/2);
 
             //Flip the weapon so it faces the same direction as the player
-            weapon.transform.rotation = transform.rotation;
+            gun.transform.rotation = transform.rotation;
+
+            //If we dont have a weapon make it our current weapon
+            if(activeWeapon == null)
+            {
+                gun.isActive = true;
+                activeWeapon = gun;
+                activeWeaponID = 0;
+            } else
+            {
+                //Otherwise hide it and keep it inactive
+                gun.GetComponent<SpriteRenderer>().enabled = false;
+            }
             
-            //Set the weapon to active so it can shoot
-            weapon.isActive = true;
-            //Set the current players active weapon to the new one
-            activeWeapon = weapon;
+            weapons.Add(numWeapons++, gun);
         }
     }
 }
